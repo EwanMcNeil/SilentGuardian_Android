05e369ff0fc0868e73667466fdcac66a32734c9a
EwanMcNeil <ewanmcneil11@gmail.com>
2020-02-26 13:57:12 -0500

    Added gps location methods

--- app/src/main/java/com/example/silentguardian_android/MainActivity.java
+++ app/src/main/java/com/example/silentguardian_android/MainActivity.java
@@ -7,12 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     protected Button profileButton;
     protected Button thresholdButton;
     protected Button bubbleButton;
+
+
+    protected TextView longitudeTextView;
+    protected TextView latitudeTextView;
+
+    messageGPSHelper gpsHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,15 @@ public class MainActivity extends AppCompatActivity {
         profileButton = findViewById(R.id.profileButton);
         thresholdButton = findViewById(R.id.thresholdButton);
         bubbleButton = findViewById(R.id.bubbleButton);
+
+        gpsHelper = new messageGPSHelper(this);
+
+        longitudeTextView = findViewById(R.id.longtextView);
+        latitudeTextView = findViewById(R.id.latTextView);
+
+        longitudeTextView.setText("Longitude: " + gpsHelper.getLong());
+        latitudeTextView.setText("Latitude: " +gpsHelper.getLat());
+
 
         profileButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/silentguardian_android/messageGPSHelper.java
@@ -0,0 +1,56 @@
+package com.example.silentguardian_android;
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+
+import androidx.core.app.ActivityCompat;
+
+
+public class messageGPSHelper {
+    private static final double TODO = 88;
+    Context mContext;
+
+    public messageGPSHelper(Context in_context){
+        this.mContext = in_context;
+    }
+
+    protected double getLong() {
+        LocationManager lm = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return TODO;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        double longitude = location.getLongitude();
+
+        return longitude;
+    }
+
+    protected double getLat() {
+        LocationManager lm = (LocationManager)  mContext.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return TODO;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double latitude = location.getLatitude();
+        return latitude;
+    }
+}
