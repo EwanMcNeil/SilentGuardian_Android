4cdcf3530f5751350e6308841f37c220c936e6dc
Broflowsky <v.puyfourcat@gmail.com>
2020-04-03 18:10:01 -0400

    renamed the audioDatabase table name from file to files File was already used for a column name as well this causes issues when retrieving sometimes

--- app/src/main/java/com/example/silentguardian_android/MainActivity.java
+++ app/src/main/java/com/example/silentguardian_android/MainActivity.java
@@ -42,11 +42,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         sharePreferenceHelper = new SharePreferenceHelper(this);
         //initialzing the sentmessage
