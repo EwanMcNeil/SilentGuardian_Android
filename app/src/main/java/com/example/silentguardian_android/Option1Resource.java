d7263fc070d4a42f821eac6ff39dfef27c50b51c
kasobellini <katebellini@gmail.com>
2020-04-11 18:20:48 -0400

    commit

--- app/src/main/java/com/example/silentguardian_android/Option1Resource.java
+++ app/src/main/java/com/example/silentguardian_android/Option1Resource.java
@@ -2,15 +2,18 @@ package com.example.silentguardian_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Option1Resource extends AppCompatActivity {
 
-    protected TextView url1_option1;
-    protected TextView url2_option1;
-    protected TextView url3_option1;
-    protected TextView url4_option1;
+    protected TextView url1_option1; /**https://cavac.qc.ca/en/*/
+    protected TextView url2_option1; /**https://www.centredecrise.ca/lescentres*/
+    protected TextView url3_option1; /**https://www.ivac.qc.ca*/
+    protected TextView url4_option1; /**http://www.ccvt.org*/
 
 
     @Override
@@ -25,10 +28,40 @@ public class Option1Resource extends AppCompatActivity {
 
 
 
+        url1_option1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            clicking("https://cavac.qc.ca/en/");
+         }
+         });
+
+        url2_option1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            clicking("https://www.centredecrise.ca/lescentres");
+        }
+        });
+
+        url3_option1.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            clicking("https://www.ivac.qc.ca");
+            }
+        });
+
+        url4_option1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clicking("http://www.ccvt.org");
+            }
+        });
 
 
+        }
 
-
-
+        public void clicking(String url){
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
     }
 }
